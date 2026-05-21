7790e3d73110c0d29d0bea9e4a8cca9c2f506b2f
milelh53-stack <mile.lh53@gmail.com>
2026-05-21 18:32:05 -0500

    Delete ejecutar_part1.bat

--- ejecutar_part1.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist ".venv\Scripts\python.exe" (
-    echo Falta .venv. Ejecute primero: fix_entorno.bat
-    pause
-    exit /b 1
-)
-".venv\Scripts\python.exe" run_part1.py %*
-pause
